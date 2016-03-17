54c6e920ed11aca012b46f5a790a86a445da43cc
AJ Bowen <aj@gandi.net>
2016-03-17 10:30:58 +0100

    fix battery charge script; sort SSIDs in addwifi.sh; add colors.sh for reference

--- addwifi.sh
+++ addwifi.sh
@@ -1,10 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $1 == "edit" ]]; then
+    echo "shifting"
+    shift
+fi
+
+if [ $1 == "show" ]; then
+    if [ -z $2 ]; then
+        nmcli connection show --active
+    else
+        nmcli connection show "$2"
+    fi
+    exit
+fi
+
+if [ $1 == "down" ]; then
+    nmcli connection show --active \
+        | tail -n1 \
+        | awk '{print $3}' \
+        | xargs nmcli connection down
+    exit $?
+fi
 
 SSID=$1
 PSK=$2
 
 if [ -z "$SSID" ]; then
-    nmcli d wifi list
+    nmcli d wifi list \
+        | sort
+    exit 0
+fi
+
+if [ "$SSID" == "tui" ]; then
+    nmtui
     exit 0
 fi
 
