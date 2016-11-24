021a32a0b9074abe7f657d131fb4d6f189ae91b6
AJ Bowen <aj@soulshake.net>
2016-11-23 22:00:43 -0600

    Add comments to slackpaste

--- slackpaste.rb
+++ slackpaste.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-
+# Adapted from:
 # https://gist.github.com/jcsalterego/8dbcebe23f5434c58a8859eaeea9a728
 # https://twitter.com/jcsalterego/status/801574032583917568
 
@@ -9,7 +9,7 @@ FORMAT = "{{author}}: {{message}}"
 TIMESTAMP_RE = /\[[0-9:A-Z ]+\]/
 DAY_MARKER_RE = /(-){5}[^-]+(-){5}/
 
-# linux
+# try to handle both linux and osx
 if (/linux/ =~ RUBY_PLATFORM) != nil
   input = `xclip -out`
   output = 'xclip -in'
@@ -18,33 +18,42 @@ elsif (/darwin/ =~ RUBY_PLATFORM) != nil
   output = 'pbcopy'
 end
 
-require 'pry'
-#binding.pry
+last_author = "???"
 
-last_author = "{MAXLEN_REPLACE}"
-
+# Read from the clipboard, remove day marker lines and split by double newlines
 lines = input.
       split(/\n/).
       reject{|line| line =~ DAY_MARKER_RE}.
       join("\n").
       split(/\n\n/)
 
+# Keep track of the length of the longest author name so we can align messages for printing later
 maxlen = 0
 
+# Figure out which lines are from a new author.
 lines = lines.map(&:strip).map do |line|
+
+  # A timestamp is displayed before each message, so use that to split into individual messages
   if line =~ TIMESTAMP_RE
     author = last_author
     words = line.split(TIMESTAMP_RE).map(&:strip).join
 
-    # line format: author <TAB> message
+    # The first message from a given author is in the following format:
+    # author <TAB> message
+    # Subsequent messages are in the same format but without the leading `author <TAB>`.
     if words.include?("\t")
+      # Transform to [author, words words words]
       words = words.split("\t", 2)
     else
-      # if there's no <tab>, and there's a newline right after the first word, maybe the author has started a message with a newline
+      # If there's no <tab> then there are two possibilities:
+      # 1. the author has started a message with a newline, or
+      # 2. This is not the first message from that author
       x = words.split("\n", 2)
       if !x.first.include?(' ')
+        # Transform to [author, words words words]
         words = words.split("\n", 2)
       else
+        # Transform to [words words words]
         words = [words]
       end
     end
@@ -64,10 +73,8 @@ lines = lines.map(&:strip).map do |line|
   {:author=>last_author, :message=>message}
 end
 
-#lines = lines.reject{|line| line.nil?}
-
 if lines.compact.empty?
-  $stderr.puts "Bailing!"
+  $stderr.puts "No slack-like lines found on your clipboard. Bailing!"
   puts input
   exit 1
 end
