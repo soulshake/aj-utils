6b7a78dfa8925185bfcd450474588ae71846351d
AJ Bowen <aj@soulshake.net>
2016-11-23 20:16:08 -0600

    update screenshot script

--- screenshot.sh
+++ screenshot.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-# scrot -q 100 '/home/aj/Pictures/Screenshots/%Y-%m-%d_%H-%M-%S_$wx$h.png' -e 's3cmd put -P $f s3://s3.j3ss.co/screenshots/ && echo https://s3.j3ss.co/screenshots/$n | xclip -i -selection clipboard'
-
-# xinit /usr/X11R6/bin/xterm
-
-#exec >/tmp/ss.$$.out
-#exec 2>/tmp/ss.$$.err
+# Take a screenshot, upload to s3 and copy the URL to the clipboard.
+# Works when executed directly, but not when executed via i3 bindsym :(
 
 source ~/.secrets/ss.aws
 
@@ -16,18 +11,13 @@ url="ss.soulshake.net/$filename"
 
 logger --id --tag "screenshot.sh" "scrotting: $filename"
 
-scrot -q 100 $1 $path
-msg="aws s3 cp --acl public-read $path s3://ss.soulshake.net # $url"
+scrot -q 100 "$path"
+aws s3 cp --acl public-read $path s3://ss.soulshake.net # ss.soulshake.net/Screenshot_2016-11-02_22:33.png
 
-#echo $url | xclip -i -selection clipboard
-echo $msg | xclip -i -selection clipboard 
+msg="aws s3 cp --acl public-read $path s3://ss.soulshake.net"
 
-#echo "ss.soulshake.net.s3-website.eu-central-1.amazonaws.com/$n"
-#scrot -q 100 $1 '/home/aj/Pictures/Screenshots/Screenshot_%Y-%m-%d_%H.%M.%S.png' -e 'aws s3 cp --acl public-read $f s3://ss.soulshake.net && echo ss.soulshake.net.s3-website.eu-central-1.amazonaws.com/$n'
+echo $url | xclip -i -selection clipboard
 
 logger --id --tag "screenshot.sh" "done scrotting: $url"
 
 i3-nagbar -m "image url copied to clipboard: $url"
-echo "done"
-
-#/usr/share/command-not-found
