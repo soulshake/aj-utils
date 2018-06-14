563f5b0942e814c04a9dab6ab377eb0f0dc90457
AJ Bowen <aj@soulshake.net>
2018-06-14 16:08:45 +0200

    shfmt addwifi.sh

--- addwifi.sh
+++ addwifi.sh
@@ -1,75 +1,74 @@
 #!/bin/bash
 
 activate_connection() {
-    conn="$1"
+  conn="$1"
 }
 
 case "$1" in
 show)
-    shift
-    if [ -z "$1" ]; then
-        nmcli connection show --active
-    else
-        nmcli connection show "$1"
-    fi
-    exit
-    ;;
+  shift
+  if [ -z "$1" ]; then
+    nmcli connection show --active
+  else
+    nmcli connection show "$1"
+  fi
+  exit
+  ;;
 edit)
-    shift
-    echo "Editing down $1"
-    nmtui edit
-    ;;
+  shift
+  echo "Editing down $1"
+  nmtui edit
+  ;;
 down)
-    shift
-    echo "Putting down $1"
-    if [ -z "$1" ]; then
-        nmcli connection show --active \
-            | tail -n1 \
-            | awk '{print $3}' \
-            | xargs nmcli connection down
-        exit $?
-    fi
-    ;;
+  shift
+  echo "Putting down $1"
+  if [ -z "$1" ]; then
+    nmcli connection show --active |
+      tail -n1 |
+      awk '{print $3}' |
+      xargs nmcli connection down
+    exit $?
+  fi
+  ;;
 help)
-    echo "$SSID" "$PSD"
-    echo "$0 [show|edit|down]"
-    ;;
+  echo "$SSID" "$PSD"
+  echo "$0 [show|edit|down]"
+  ;;
 tui)
-    nmtui
-    ;;
+  nmtui
+  ;;
 *)
-    # nmcli c up w-${SSID}
-    SSID=$1
-    PSK=$2
-    if [ -z "$SSID" ]; then
-        nmcli d wifi list
-        exit 0
-    fi
+  # nmcli c up w-${SSID}
+  SSID=$1
+  PSK=$2
+  if [ -z "$SSID" ]; then
+    nmcli d wifi list
+    exit 0
+  fi
 
-    if [ -e "/etc/NetworkManager/system-connections/$SSID" ]; then
-        echo "Found existing network:"
-        sudo egrep '^ssid=|^psk=' "/etc/NetworkManager/system-connections/$SSID"
-        conn="$(nmcli conn | grep -i "${SSID}" | head -n1 | awk '{print $1}')"
-
-        if [ -z "$PSK" ]; then
-            echo "Activating conn ${conn} (SSID $SSID) with no key..."
-            nmcli conn up "$conn"
-        else
-            echo "key was provided; adding new connection!"
-        fi
-    fi
+  if [ -e "/etc/NetworkManager/system-connections/$SSID" ]; then
+    echo "Found existing network:"
+    sudo egrep '^ssid=|^psk=' "/etc/NetworkManager/system-connections/$SSID"
+    conn="$(nmcli conn | grep -i "${SSID}" | head -n1 | awk '{print $1}')"
 
     if [ -z "$PSK" ]; then
-        echo "Adding SSID $SSID with no key..."
-        nmcli dev wifi connect "$SSID"
+      echo "Activating conn ${conn} (SSID $SSID) with no key..."
+      nmcli conn up "$conn"
+    else
+      echo "key was provided; adding new connection!"
     fi
-    if [ -n "$PSK" ]; then
-        echo "Adding SSID $SSID with PSK $PSK..."
-        nmcli dev wifi connect "$SSID" password "$PSK"
-    fi
+  fi
 
-    ;;
+  if [ -z "$PSK" ]; then
+    echo "Adding SSID $SSID with no key..."
+    nmcli dev wifi connect "$SSID"
+  fi
+  if [ -n "$PSK" ]; then
+    echo "Adding SSID $SSID with PSK $PSK..."
+    nmcli dev wifi connect "$SSID" password "$PSK"
+  fi
+
+  ;;
 esac
-
 
 echo "done"
